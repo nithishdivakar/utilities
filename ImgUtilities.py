d2bd265f7614eecc421dec3eb6574e7ad6a5ba56
nithishdivakar <nd@gmail.com>
2015-12-21 12:18:54 +0530

    Some code reuse

--- ImgUtilities.py
+++ ImgUtilities.py
@@ -38,24 +38,19 @@ def pad_symmetric(img,dim):
 	return skutil.pad(img,pad_width = dim,mode='symmetric')
 
 
-def extract_patches(img,patch_shape,stride):
-	img = np.array(img)
-	patches = skshape.view_as_windows(img, window_shape=patch_shape,step = stride)
-	print patches.shape
-	pat = np.concatenate(patches,axis = 0)
-	return pat
-
-
 def deconvolve_patches(img,patch_shape,stride):
 	img = np.array(img)
 	patches = skshape.view_as_windows(img, window_shape=patch_shape,step = stride)
 	return patches
 
+def extract_patches(img,patch_shape,stride):
+	patches = deconvolve_patches(img,patch_shape,stride)
+	pat = np.concatenate(patches,axis = 0)
+	return pat
 
 def explode_patches(img,patch_shape,stride):
 	'''all rectangles are written as height x width'''
-	img = np.array(img)
-	patches = skshape.view_as_windows(img, window_shape=patch_shape,step = stride)
+	patches = deconvolve_patches(img,patch_shape,stride)
 	pat = []
 	for x in xrange(patches.shape[0]):
 		pat.append(np.hstack(patches[x]))
