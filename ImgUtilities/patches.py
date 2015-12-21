52e8b8e667ef0d1e818383b705de6984cc745556
nithishdivakar <nd@gmail.com>
2015-12-21 15:46:39 +0530

    patches new styles

--- ImgUtilities/patches.py
+++ ImgUtilities/patches.py
@@ -8,7 +8,7 @@ def deconvolve_patches(img,patch_shape,stride):
 	patches = skshape.view_as_windows(img, window_shape=patch_shape,step = stride)
 	return patches
 
-def extract_patches(img,patch_shape,stride):
+def list_patches(img,patch_shape,stride):
 	patches = deconvolve_patches(img,patch_shape,stride)
 	pat = np.concatenate(patches,axis = 0)
 	return pat
@@ -21,3 +21,24 @@ def explode_patches(img,patch_shape,stride):
 		pat.append(np.hstack(patches[x]))
 	pat = np.vstack(pat)
 	return pat
+
+def list_to_deconvolve(patches_train,shape):
+	k = 0;
+	pat_shape = patches_train[0].shape
+	img_deconvolve = np.zeros((shape[0],shape[1],pat_shape[0],pat_shape[1]))
+	for x in xrange(shape[0]):
+		img_deconvolve[x,:] = patches_train[k:k+shape[1]]
+		k = k+shape[0]
+	return img_deconvolve
+
+def deconvolve_to_explode(patches_deconv):
+	pat = []
+	for x in xrange(patches_deconv.shape[0]):
+		pat.append(np.hstack(patches_deconv[x]))
+	pat = np.vstack(pat)
+	return pat
+
+def list_to_explode(patches_train,shape):
+	return deconvolve_to_explode(
+	      list_to_deconvolve(patches_train,shape)
+	)
--- test.py
+++ test.py
@@ -5,10 +5,25 @@ import numpy as np
 #from ImgUtilities.error import psnr
 #from ImgUtilities.error import ssim
 
-from ImgUtilities import similarity_measures as E
+from ImgUtilities import similarity as E
+from ImgUtilities import patches as P
 
 lena_gray = Image.open("testimg/lena_gray.jpg")
 lena_color = Image.open("testimg/lena_color.gif")
+
+pat1 = P.list_patches(lena_gray,(11,11),1)
+print pat1.shape
+pat2 = P.deconvolve_patches(lena_gray,(11,11),1)
+print pat2.shape
+pat3 = P.list_to_deconvolve(pat1,(pat2.shape[0],pat2.shape[1]))
+pat4 = P.list_to_explode(pat1,(pat2.shape[0],pat2.shape[1]))
+pat5 = P.explode_patches(lena_gray,(11,11),1)
+
+print pat2.shape
+print pat3.shape
+
+print np.array_equal(pat2,pat3)
+print np.array_equal(pat4,pat5)
 
 
 print E.mse(lena_gray,lena_gray)
