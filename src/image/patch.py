84529e05284c7037ffd4d44a80b14f75462f24b7
nithishdivakar <nd@gmail.com>
2016-01-11 14:09:29 +0530

    modified patch.py

--- src/image/patch.py
+++ src/image/patch.py
@@ -1,56 +1,57 @@
 from PIL import Image
-import numpy as np
+import numpy
 from skimage.util import shape as skshape
+import padding 
+from itertools import product
 
-	
-def deconvolve_patches(img,patch_shape,stride):
-	img = np.array(img)
-	patches = skshape.view_as_windows(img, window_shape=patch_shape,step = stride)
+
+def patch_dimen(patch_radius):
+	if len(patch_radius)==1:
+		patch_shape = (2*patch_radius+1,2*patch_radius+1)
+	else:
+		patch_shape = (2*patch_radius[0]+1,2*patch_radius[1]+1)
+	return patch_shape
+
+def convert(source_format,target_format,input_datum,**kwargs):
+	method_dicnry={
+		'patch-image': convert_patch_image,
+		'image-patch': convert_image_patch,
+	}
+	# ToDO: check error
+	return method_dicnry[source_format+"-"+target_format](input_datum,**kwargs)
+
+
+
+def convert_image_patch(input_datum,patch_radius,stride=(1,1),pad=True):
+	img = numpy.array(input_datum)
+	if pad == True:
+		img = padding.pad_symmetric(img,patch_radius)
+	patch_shape = patch_dimen(patch_radius)
+	patches = skshape.view_as_windows(img,window_shape = patch_shape,step = stride)
+	patches = numpy.concatenate(patches,axis = 0)
 	return patches
 
-def list_patches(img,patch_shape,stride):
-	patches = deconvolve_patches(img,patch_shape,stride)
-	pat = np.concatenate(patches,axis = 0)
-	return pat
+def convert_patch_image(input_datum,image_size,stride=(1,1),pad=True):
+	patches = input_datum
+	patch_shape = patches[0].shape
+	if pad == True:
+		outpt_shape = (image_size[0] + patch_shape[0]-1, image_size[1] + patch_shape[1]-1)
+	else:
+		outpt_shape = image_size
 
-def explode_patches(img,patch_shape,stride):
-	'''all rectangles are written as height x width'''
-	patches = deconvolve_patches(img,patch_shape,stride)
-	pat = []
-	for x in xrange(patches.shape[0]):
-		pat.append(np.hstack(patches[x]))
-	pat = np.vstack(pat)
-	return pat
-
-def list_to_deconvolve(patches_train,shape):
-	k = 0;
-	pat_shape = patches_train[0].shape
-	img_deconvolve = np.zeros((shape[0],shape[1],pat_shape[0],pat_shape[1]))
-	for x in xrange(shape[0]):
-		img_deconvolve[x,:] = patches_train[k:k+shape[1]]
-		k = k+shape[0]
-	return img_deconvolve
-
-def deconvolve_to_explode(patches_deconv):
-	pat = []
-	for x in xrange(patches_deconv.shape[0]):
-		pat.append(np.hstack(patches_deconv[x]))
-	pat = np.vstack(pat)
-	return pat
-
-def list_to_explode(patches_train,shape):
-	return deconvolve_to_explode(
-	      list_to_deconvolve(patches_train,shape)
-	)
-
-
-def deconvolve_to_img(patches_deconv):
-	imgsize = (patches_deconv.shape[0]+patches_deconv.shape[2],patches_deconv.shape[1]+patches_deconv.shape[3])
-	W =np.zeros(imgsize)
-	I =np.zeros(imgsize)
-	for x in xrange(patches_deconv.shape[0]):
-		for y in xrange(patches_deconv.shape[1]):
-			I[x:x+11,y:y+11] += patches_deconv[x,y]
-			W[x:x+11,y:y+11] += 1.0 
-	I = I/W
-	return I
+	image = numpy.zeros(outpt_shape)
+	weigh = numpy.zeros(outpt_shape)
+	k = 0
+	for coord in product( xrange(0,outpt_shape[1]-patch_shape[1]+1,stride[1]), xrange(0,outpt_shape[0]-patch_shape[0]+1,stride[0])):
+		#print k," ",
+		image[coord[0]:coord[0]+patch_shape[0],coord[1]:coord[1]+patch_shape[1]] += patches[k]
+		weigh[coord[0]:coord[0]+patch_shape[0],coord[1]:coord[1]+patch_shape[1]] += 1.0
+		k    = k+1
+	image = image/weigh
+	if pad == True:
+		image = image[
+		       ((patch_shape[0]-1)/2):((patch_shape[0]-1)/2) + image_size[0],
+		       ((patch_shape[1]-1)/2):((patch_shape[1]-1)/2) + image_size[1]
+		]
+	return image
+	
